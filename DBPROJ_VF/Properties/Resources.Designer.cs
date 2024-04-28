989b7157fb1a755e5813448b9e1dc29c551c0d87
atifibrahim7 <bigdream382@gmail.com>
2024-04-28 17:33:57 +0500

    Trainer register

--- DBPROJ_VF/Properties/Resources.Designer.cs
+++ DBPROJ_VF/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace DBPROJ_VF.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Trainer {
+            get {
+                object obj = ResourceManager.GetObject("Trainer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }

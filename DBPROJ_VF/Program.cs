3a26db461c9af5eb08520a276f4ab8d584661dff
saadnadeemsls@gmail.com <saadnadeemsls@gmail.com>
2024-04-30 22:21:58 +0500

    Trainer Feedback form added

--- DBPROJ_VF/Program.cs
+++ DBPROJ_VF/Program.cs
@@ -16,7 +16,7 @@ namespace DBPROJ_VF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrainerAppointment() );
+            Application.Run(new TrainerFeedback() );
         }
     }
 }
---
+++ DBPROJ_VF/TrainerFeedback.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DBPROJ_VF
+{
+    partial class TrainerFeedback
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrainerFeedback));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TrainerFeedbackGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TrainerFeedbackGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(27, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trainer Feedback";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(597, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Overall Rating:";
+            // 
+            // TrainerFeedbackGrid
+            // 
+            this.TrainerFeedbackGrid.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TrainerFeedbackGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TrainerFeedbackGrid.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.TrainerFeedbackGrid.Location = new System.Drawing.Point(32, 78);
+            this.TrainerFeedbackGrid.Name = "TrainerFeedbackGrid";
+            this.TrainerFeedbackGrid.RowHeadersWidth = 51;
+            this.TrainerFeedbackGrid.RowTemplate.Height = 24;
+            this.TrainerFeedbackGrid.Size = new System.Drawing.Size(529, 392);
+            this.TrainerFeedbackGrid.TabIndex = 2;
+            // 
+            // TrainerFeedback
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(942, 482);
+            this.Controls.Add(this.TrainerFeedbackGrid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TrainerFeedback";
+            this.Text = "TrainerFeedback";
+            ((System.ComponentModel.ISupportInitialize)(this.TrainerFeedbackGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView TrainerFeedbackGrid;
+    }
+}
